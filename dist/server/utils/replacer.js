9cc23476b0645e57bda5c49360a8beb6fe0bcbd3
IMRAN BAALI <56740152+kirwako@users.noreply.github.com>
2024-04-17 09:11:05 +0100

    publish version 1.0.2 that use .env to get media prefix

--- dist/server/utils/replacer.js
+++ dist/server/utils/replacer.js
@@ -1,18 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.replaceUploadsWithMediaPrefix = void 0;
-const MEDIA_PREFIX = strapi.config.get("server.url", "http://localhost:1337") + "/uploads/";
+let MEDIA_PREFIX = 'http://localhost:1337';
+if (process.env.MEDIA_PREFIX) {
+    MEDIA_PREFIX = process.env.MEDIA_PREFIX;
+}
+else {
+    MEDIA_PREFIX = strapi.config.get('server.url', 'http://localhost:1337');
+}
+MEDIA_PREFIX += '/uploads/';
 const replaceUploadsWithMediaPrefix = (obj) => {
     if (Array.isArray(obj)) {
         for (let i = 0; i < obj.length; i++) {
             obj[i] = (0, exports.replaceUploadsWithMediaPrefix)(obj[i]);
         }
     }
-    else if (typeof obj === "object" && obj !== null) {
+    else if (typeof obj === 'object' && obj !== null) {
         for (let key in obj) {
-            if (typeof obj[key] === "string" &&
-                obj[key].startsWith("/uploads") &&
-                key === "url") {
+            if (typeof obj[key] === 'string' &&
+                obj[key].startsWith('/uploads') &&
+                key === 'url') {
                 obj[key] = obj[key].replace(/\/uploads\//g, MEDIA_PREFIX);
             }
             else {
